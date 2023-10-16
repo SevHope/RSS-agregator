859b47918b0a9e8c849ed3ff96dfc3fb6b2fd0ba
Nadezhda Sevastianova <s.hope@bk.ru>
2023-10-16 13:52:24 +0300

    add webpack

---
+++ Makefile
@@ -0,0 +1,14 @@
+develop:
+	npx webpack serve
+
+install:
+	npm ci
+
+build:
+	NODE_ENV=production npx webpack
+
+test:
+	npm test
+
+lint:
+	npx eslint .
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    mode: process.env.NODE_ENV || 'development',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.scss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: 'url-loader?limit=10000',
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+          use: 'file-loader',
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+    ],
+  };
